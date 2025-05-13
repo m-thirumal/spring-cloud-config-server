c09594ab14bb5781653174661c4c9da6eef17c79
ThirumalM <ThirumalM@NESL-THIRUMALM>
2025-05-13 21:32:00 +0530

    Security is added

--- README.md
+++ README.md
@@ -1,42 +1,61 @@
 # spring-cloud-config-server
+
 ### What is this repository for? ###
-* 0.1
+
 * Externalized (Property files)
 * Environment specific (Spring Profile)
 * Consistent (Spring cloud config)
 * Version Management (GIT repo source)
 * Real Time Management [Refresh config properties in client without restart]
 
-
 ## Required Repos link
 
 * [https://github.com/M-Thirumal/config-repo](https://github.com/M-Thirumal/config-repo)
 * [https://github.com/M-Thirumal/spring-cloud-config-client](https://github.com/M-Thirumal/spring-cloud-config-client)
 
-### How do I get set up? ###
+## Eclipse set up
 
-* Summary of set up
-* Configuration
-* Dependencies
+* Import as maven project
+* Run as `Spring boot` application
 
-    * Spring Boot
-    * Git
-    * Maven 
-    * Any IDE
+## Build 
 
-* Database configuration
-* How to run tests
-* Deployment instructions
+```
+mvn clean package
+```
 
-### Contribution guidelines ###
+## Run as Jar 
 
-* Writing tests
-* Code review
-* Other guidelines
+```
+java -jar .\target\spring-cloud-config-server-0.0.1-SNAPSHOT.jar
+```
 
-### What not to commit ####
+## End points
 
-* src/main/resources/application.properties file
+Application in `DEV` environment will not ask for credentials, however on `PRO` environment it will and it;s basic authentication.
+
+```
+http://localhost:8888/{application}/{profile}/{label}
+```
+
+## Encryption for properties
+To encrypt the value before it gets stored in database/git
+
+```
+curl localhost:8888/encrypt -s -d mysecret
+```
+
+or 
+
+```
+curl -u <username>:<password> -s -d <data> http://localhost:8888/encrypt
+```
+
+## Decryption for properties
+
+```
+curl localhost:8888/decrypt -s -d 682bc583f4641835fa2db009355293665d2647dade3375c0ee201de2a49f7bda
+```
 
 ### Who do I talk to? ###
 * Thirumal
--- src/main/java/com/thirumal/config/SecurityConfig.java
+++ src/main/java/com/thirumal/config/SecurityConfig.java
@@ -3,31 +3,78 @@
  */
 package com.thirumal.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author Thirumal
- *
+ * Web security is enabled for the purpose of 
+ * 	- encrypting/de-crypting the properties values that are encrypted at-rest
+ * 	- Securing the application
  */
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity//(debug = true)
 public class SecurityConfig { //extends WebSecurityConfigurerAdapter {
-	/*
-	@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
-			.withUser("thirumal").password("thirumal").roles("ADMIN").roles("ACTUATOR");
-    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().hasRole("ADMIN").and().httpBasic().and().csrf().disable();
+	/**
+	 * Development security: permit all
+	 * @param httpSecurity
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	@Profile("DEV") 
+    SecurityFilterChain developmentSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/**").permitAll() //To disable login
+            )        	
+            .csrf(csrf -> csrf.disable())
+            .formLogin(form -> form.disable()) //To disable login
+             .httpBasic(httpBasic -> httpBasic.disable()); //To disable login
+        return httpSecurity.build();
     }
-    
-	@Bean 
-	public PasswordEncoder passwordEncoder() { 
-		PasswordEncoder encoder = new BCryptPasswordEncoder(); 
-		return encoder; 
-	}*/
+	
+	/**
+	 * Production security: require authentication
+	 * @param httpSecurity
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	@Profile("PRO")
+    SecurityFilterChain productionSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .authorizeHttpRequests(authz -> authz
+               .anyRequest().authenticated()
+            )        	
+            .csrf(csrf -> csrf.disable())
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+	
+	@Bean
+	InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+		UserDetails user1 = User.withUsername("admin").password(passwordEncoder.encode("admin")) 
+				.roles("ADMIN").build();
+		return new InMemoryUserDetailsManager(user1);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+
 	
 }
